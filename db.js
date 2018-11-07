6612519d5c5c0abf9f644c31c20cc6230332ea26
Vladyslav Parashchenko <mrfenix2015@gmail.com>
2018-11-07 17:57:51 +0200

    add functions for export to db

--- db.js
+++ db.js
@@ -1,20 +1,86 @@
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
- 
-
-let sqlite3 = require('sqlite3').verbose();
-let db_file_name = 'olympic_history.db';
+let sqlite3 = require("sqlite3").verbose();
+let db_file_name = "olympic_history.db";
 let db = new sqlite3.Database(db_file_name);
 
 module.exports = {
-    saveUser: function(user) {
-        
-        db.run(`INSERT INTO langs(name) VALUES(?)`, ['C'], function(err) {
-            if (err) {
-              return console.log(err.message);
-            }
-            // get the last insert id
-            console.log(`A row has been inserted with rowid ${this.lastID}`);
-          });
-    },
+  saveToDb: function(objectWithData) {
+    db.serialize(function() {
+      db.run(
+        `INSERT INTO sports (name) VALUES ('${objectWithData["sport"]}');`,
+        handleDbError
+      );
+      db.run(
+        `INSERT INTO events (name) VALUES ("${objectWithData["event"]}");`,
+        handleDbError
+      );
+      db.run(
+        `INSERT INTO games (year, season, city) VALUES ('${
+          objectWithData["year"]
+        }', 
+            '${objectWithData["season"]}','${objectWithData["city"]}');`,
+        handleDbError
+      );
+      db.run(
+        `INSERT INTO teams (name, noc_name) VALUES ('${
+          objectWithData["team"]
+        }', '${objectWithData["noc"]}');`,
+        handleDbError
+      );
+      db.run(
+        `INSERT INTO athletes (id, full_name, sex, year_of_birth,  team_id, params) VALUES 
+            ('${objectWithData["id"]}', '${objectWithData["name"]}', '${
+          objectWithData["sex"]
+        }', '${objectWithData["year_of_birth"]}', 
+            (select id from teams where name='${
+              objectWithData["team"]
+            }' and noc_name='${objectWithData["noc"]}'), '${
+          objectWithData["params"]
+        }');`,
+        handleDbError
+      );
+      db.run(
+        `INSERT INTO results (athlete_id, sport_id, event_id, game_id, medal) VALUES 
+            (
+                (SELECT id from athletes where full_name='${
+                  objectWithData["name"]
+                }' and year_of_birth='${
+          objectWithData["year_of_birth"]
+        }' and params='${objectWithData["params"]}' and sex='${
+          objectWithData["sex"]
+        }'),
+                (SELECT id from sports where name="${objectWithData["sport"]}"),
+                (SELECT id from events where name="${objectWithData["event"]}"),
+                (SELECT id from games where year='${
+                  objectWithData["year"]
+                }' AND season='${objectWithData["season"]}' and city='${
+          objectWithData["city"]
+        }'),
+                '${objectWithData["medal"]}'
+            );`,
+        handleDbError
+      );
+    });
+  },
+  saveCollectionToDb: function(collection, tableName) {
+    let sql = `INSERT INTO ${tableName} (name) VALUES `;
+    collection.forEach(function(item) {
+      sql+= `("${item.replace("'", "\'")}"), `;
+    });
+    console.log(sql);
+    sql = sql.substring(0, sql.length - 2) + ";";
+    db.run(sql);
+  },
+
+  saveGamesToDb: function(collection) {
+      let sql = 'INSERT INTO ${tableName} (year, season, city) VALUES ';
+      collection.forEach(function(item) {
+        let data = item.split("-");  
+        console.log(data);
+        sql+= `('${data[0]}','${data[1]}','${data[2]}')`;
+      });  
+  }
 };
+
+function handleDbError(error) {
+  //   console.log(error);
+}
--- export_csv_to_db.js
+++ export_csv_to_db.js
@@ -1,16 +1,95 @@
-const fs = require('fs');
-const readline = require('readline');
-
+const fs = require("fs");
+const readline = require("readline");
+const db = require("./db.js");
 let filename = process.argv.slice(2)[0];
-console.log(process.argv.slice(2));
 
 let rd = readline.createInterface({
-    input: fs.createReadStream(filename),
-    output: process.stdout
+  input: fs.createReadStream(filename)
 });
-rd.on('error', function(error){
-    console.log(error.message);
-})
-rd.on('line', function(line) {
-    console.log(line);
+
+let sports = new Set();
+let events = new Set();
+let games = new Set();
+let users = [];
+rd.on("line", function(line) {
+  let columns = line.split(",");
+  if (columns[0] != "ID") {
+    object = makeObject(columns);
+    sports.add(object["sport"]);
+    object["sport_id"] = sports.size;
+    events.add(object["event"]);
+    object["event_id"] = events.size;
+    users.push(object);
+    game = object["year"] + "-" + object["season"] + "-" + object["city"];
+    // console.log(game);
+    games.add(game);
+    object["game_id"] = games.size;
+
+    // if (object["id"] != "ID") {
+    //   db.saveToDb(object);
+    // }
+  }
 });
+rd.on("close", function() {
+  db.saveCollectionToDb(sports, "sports");
+  // db.saveCollectionToDb(events, "events");
+  // db.saveGamesToDb(games);
+});
+
+function makeObject(arrWithValues) {
+  let keys = [
+    "id",
+    "name",
+    "sex",
+    "age",
+    "height",
+    "weight",
+    "team",
+    "noc",
+    "games",
+    "year",
+    "season",
+    "city",
+    "sport",
+    "event",
+    "medal"
+  ];
+  let object = {};
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    let value = arrWithValues[i] == "NA" ? null : arrWithValues[i].replace(/"/g,"");
+    if (value != null && ["name", "team"].indexOf(key) != -1) {
+      value = correctValue(key, value);
+    }
+    object[key] = value;
+  }
+  return handleObject(object);
+}
+
+function correctValue(key, value) {
+  const regexByKeys = {
+    name: /\(.*\)/,
+    team: /\-.*/
+  };
+
+  return removeSubstringByRegex(regexByKeys[key], value);
+}
+
+function removeSubstringByRegex(regularExpression, string) {
+  return string.replace(regularExpression, "");
+}
+
+function handleObject(object) {
+  let age = object["age"];
+  object["year_of_birth"] = age == null ? new Date().getFullYear() - age : age;
+  object["params"] = JSON.stringify({
+    height: object["height"],
+    weight: object["weight"]
+  });
+  object["season"] = object["season"] == "summer" ? 0 : 1;
+  delete object["age"];
+  delete object["weight"];
+  delete object["height"];
+
+  return object;
+}
