77b507473253347fce49b1d19c8ccd4bd26acc10
Vladyslav Parashchenko <mrfenix2015@gmail.com>
2018-11-08 18:09:11 +0200

    add multiple db insert

--- db.js
+++ db.js
@@ -64,20 +64,77 @@ module.exports = {
   saveCollectionToDb: function(collection, tableName) {
     let sql = `INSERT INTO ${tableName} (name) VALUES `;
     collection.forEach(function(item) {
-      sql+= `("${item.replace("'", "\'")}"), `;
+      sql += `("${item.replace("'", "'")}"), `;
     });
-    console.log(sql);
     sql = sql.substring(0, sql.length - 2) + ";";
     db.run(sql);
   },
 
-  saveGamesToDb: function(collection) {
-      let sql = 'INSERT INTO ${tableName} (year, season, city) VALUES ';
-      collection.forEach(function(item) {
-        let data = item.split("-");  
-        console.log(data);
-        sql+= `('${data[0]}','${data[1]}','${data[2]}')`;
-      });  
+  saveGamesToDb: function(games,callback) {
+    let sql = `INSERT INTO games (year, season, city) VALUES `;
+    for (let item in games) {
+      let data = item.split("-");
+      let cities = Array.from(games[item]).join(",");
+      if (data[0] != "1906 Summer" && item != "1906-0") {
+        sql += `('${data[0]}','${data[1]}',"${cities}"), `;
+      }
+    }
+    sql = sql.substring(0, sql.length - 2) + ";";
+    db.run(sql, function(err){
+      if(err){
+
+      }else{
+        callback();
+      }
+    });
+  },
+
+  saveTeamToDb: function(teams,callback) {
+    let sql = `INSERT INTO teams (name, noc_name) VALUES `;
+    for (let item in teams) {
+      if (item.length >= 3) {
+        sql += `("${teams[item]}", "${item}"), `;
+      }
+    }
+    sql = sql.substring(0, sql.length - 2) + ";";
+    db.run(sql, function(err){
+        if(err){}else{
+          callback();
+        }
+    });
+  }, 
+  saveUsersToDb: function(users) {
+    let i, j, temparray, chunk = 50;
+    for (i = 0, j = users.length; i < j; i += chunk) {
+      temparray = users.slice(i, i + chunk);
+      let sql = `INSERT INTO athletes (full_name, sex, year_of_birth, params, team_id) VALUES `;
+      temparray.forEach(function(item) {
+        sql += `("${item["name"]}", '${item["sex"]}','${item["year_of_birth"]}',
+      '${item["params"]}', (select id from teams where noc_name='${
+          item["noc"]
+        }')), `;
+      });
+      sql = sql.substring(0, sql.length - 2) + ";";
+      db.run(sql);
+    }
+  },
+  saveResultToDb: function(users) {
+    let i, j, temparray, chunk = 50;
+    for (i = 0, j = users.length; i < j; i += chunk) {
+      temparray = users.slice(i, i + chunk);
+      let sql = `INSERT INTO results (athlete_id, game_id, sport_id, event_id, medal) VALUES `;
+      temparray.forEach(function(item) {
+        sql += `(
+          (select id from athletes where full_name="${item["name"]}" and sex="${item["sex"]}" and params='${item["params"]}'), 
+         (select id from games where year='${item["year"]}' and season='${item["season"]}'),
+        (select id from sports where name="${item["sport"]}"),
+        (select id from events where name="${item["event"]}"),
+        "${item['medal']}"),`;
+      });
+      sql = sql.substring(0, sql.length - 1) + ";";
+      // console.log(sql);
+       db.run(sql);
+    }
   }
 };
 
--- export_csv_to_db.js
+++ export_csv_to_db.js
@@ -9,31 +9,25 @@ let rd = readline.createInterface({
 
 let sports = new Set();
 let events = new Set();
-let games = new Set();
+let games = {};
+let teams = {};
 let users = [];
 rd.on("line", function(line) {
-  let columns = line.split(",");
-  if (columns[0] != "ID") {
+  let columns = CSVtoArray(line);
+  if (columns != null && columns[0] != "ID") {
     object = makeObject(columns);
     sports.add(object["sport"]);
-    object["sport_id"] = sports.size;
     events.add(object["event"]);
-    object["event_id"] = events.size;
+    teams[object["noc"]] = object["team"];
     users.push(object);
-    game = object["year"] + "-" + object["season"] + "-" + object["city"];
-    // console.log(game);
-    games.add(game);
-    object["game_id"] = games.size;
-
-    // if (object["id"] != "ID") {
-    //   db.saveToDb(object);
-    // }
   }
 });
 rd.on("close", function() {
   db.saveCollectionToDb(sports, "sports");
-  // db.saveCollectionToDb(events, "events");
-  // db.saveGamesToDb(games);
+  db.saveCollectionToDb(events, "events");
+  db.saveGamesToDb(games, db.saveTeamToDb(teams,db.saveUsersToDb(users)));
+
+  // db.saveResultToDb(users);
 });
 
 function makeObject(arrWithValues) {
@@ -57,7 +51,8 @@ function makeObject(arrWithValues) {
   let object = {};
   for (let i = 0; i < keys.length; i++) {
     let key = keys[i];
-    let value = arrWithValues[i] == "NA" ? null : arrWithValues[i].replace(/"/g,"");
+    let value =
+      arrWithValues[i] == "NA" ? null : arrWithValues[i].replace(/"/g, "");
     if (value != null && ["name", "team"].indexOf(key) != -1) {
       value = correctValue(key, value);
     }
@@ -86,10 +81,54 @@ function handleObject(object) {
     height: object["height"],
     weight: object["weight"]
   });
-  object["season"] = object["season"] == "summer" ? 0 : 1;
+  object["season"] = object["season"] == "Summer" ? 0 : 1;
+  object["medal"] = medalEnumValue(object["medal"]);
   delete object["age"];
   delete object["weight"];
   delete object["height"];
-
+  olympData = handleGame(object);
   return object;
 }
+
+function medalEnumValue(value) {
+  switch (value) {
+    case "Gold":
+      return 1;
+    case "Silver":
+      return 2;
+    case "Bronze":
+      return 3;
+    default:
+      return 0;
+  }
+}
+
+function handleGame(olympData) {
+  let key = olympData["year"] + "-" + olympData["season"];
+  games[key] = games[key] || new Set();
+  games[key].add(olympData["city"]);
+  return olympData;
+}
+
+function CSVtoArray(text) {
+  // var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
+  var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g;
+  // Return NULL if input string is not well formed CSV string.
+
+  var a = []; // Initialize array to receive values.
+  text.replace(
+    re_value, // "Walk" the string using replace with callback.
+    function(m0, m1, m2, m3) {
+      // Remove backslash from \' in single quoted values.
+      if (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"));
+      // Remove backslash from \" in double quoted values.
+      else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'));
+      else if (m3 !== undefined) a.push(m3);
+      return ""; // Return empty string.
+    }
+  );
+  // Handle special case of empty last value.
+  if (/,\s*$/.test(text)) a.push("");
+  return a;
+}
+// }
