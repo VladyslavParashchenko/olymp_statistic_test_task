c20a2be28ab42b0ac67a955a9cc0129a4db4cc8c
VladyslavParashchenko <mrfenix2015@gmail.com>
2018-11-09 23:04:29 +0200

    add promise to db query and simple display for first chart

--- chart_builder.js
+++ chart_builder.js
@@ -4,24 +4,41 @@ module.exports = class ChartBuilder {
   constructor(chartParams) {
     this.chartParams = chartParams;
     this.db = new DB();
+    this.maxValue;
   }
 
   buildChartByDataFromDB() {
-    this.dataFromDb = this.db.selectMedalsStat(this.chartParams);
-    console.log(this.dataFromDb);
-    let db_request = this.db.selectMaxMedalsStat();
-    console.log(db_request);
-    this.display();
+    this.dataFromDb = this.db.selectMedalsStat(this.chartParams)
+      .then(
+        response => {
+          this.dataFromDb = response;
+          return this.db.selectMaxMedalsStat();
+        }
+      )
+      .then(
+        response => {
+          console.log(response[0]['max_count']);
+          this.maxValue = response[0]['max_count'];
+          return this.handleDataFromDb();
+        }
+      ).catch(
+        error => console.log(error.message)
+      );
   }
-  display() {}
+  display() {
+    this.dataFromDb.forEach(item => {
+      console.log(item['year']+ ''.padEnd(' ', 5)+''.padStart(item['relative_value'], '-'));
+    });
+  }
 
   handleDataFromDb() {
-    let maxRelaviteValue = 200  
-    this.dataFromDb = this.dataFromDb.map(function(item) {
-        let relativeValue = Math.ceil((item["count"] * maxRelaviteValue)/this.maxValue);
-        item['relative_value'] = relativeValue;
-        return item;
+    let maxRelaviteValue = 200;
+    this.dataFromDb = this.dataFromDb.map((item) => {
+      let relativeValue = Math.ceil((item["count"] * maxRelaviteValue) / this.maxValue);
+      item['relative_value'] = relativeValue;
+      return item;
     });
     console.log(this.dataFromDb);
+    this.display();
   }
 };
--- db.js
+++ db.js
@@ -6,31 +6,34 @@ module.exports = class DB {
     this.db = new sqlite3.Database(db_file_name);
   }
 
-  runQuery(query){
-    this.db.all(query, (err, rows) => {
-      if (err) {
-        console.log(err.message);
-      } else {
-        console.log(rows);
-        return rows;
-      }
+  runQuery(query) {
+    return new Promise((resolve, reject) =>{
+      this.db.all(query, (err, rows) => {
+        if (err) {
+          reject(err);
+          // console.log(err.message);
+        } else {
+          // console.log(rows);
+          resolve(rows);
+        }
+      });
     });
   }
 
   selectMedalsStat(params) {
-    let sql = `SELECT  year, count(*) count FROM results r INNER JOIN athletes a on a.id=r.athlete_id 
-    INNER JOIN teams t on t.id=a.team_id INNER JOIN games g on g.id=r.game_id ${this.buildRestictionsByParams(
-      params
-    )} GROUP BY year order by year desc;`;
+    let sql = `SELECT  year, count(r.id) count FROM games g LEFT JOIN results r on g.id=r.game_id LEFT JOIN athletes a on a.id=r.athlete_id 
+    LEFT JOIN teams t on t.id=a.team_id  ${this.buildRestictionsByParams(
+        params
+      )} GROUP BY year order by year desc;`;
     console.log(sql);
     return this.runQuery(sql);
   }
 
   selectMaxMedalsStat(params) {
-    let sql = `SELECT max(count) max_count from(SELECT  year, count(*) count FROM results r INNER JOIN athletes a on a.id=r.athlete_id 
-    INNER JOIN teams t on t.id=a.team_id INNER JOIN games g on g.id=r.game_id ${this.buildRestictionsByParams(
-      params
-    )} GROUP BY year order by year desc);`;
+    let sql = `SELECT max(count) max_count from(SELECT  year, count(*) count FROM games g LEFT JOIN results r on g.id=r.game_id LEFT JOIN athletes a on a.id=r.athlete_id 
+    LEFT JOIN teams t on t.id=a.team_id  ${this.buildRestictionsByParams(
+        params
+      )} GROUP BY year order by year desc);`;
     console.log(sql);
     return this.runQuery(sql);
   }
@@ -40,12 +43,12 @@ module.exports = class DB {
     let restrictions = [];
     for (let key in params) {
       if (key != "char_type" && params[key] != null) {
-          restrictions.push(key+`="${params[key]}"`);
+        restrictions.push(key + `="${params[key]}"`);
       }
     }
     console.log(restrictions);
-    if(restrictions.length != 0){
-      return "where" +restrictions.join(" and ")
+    if (restrictions.length != 0) {
+      return "where" + restrictions.join(" and ")
     }
     return "";
   }
